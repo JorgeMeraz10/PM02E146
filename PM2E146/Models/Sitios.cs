161aa4bacaeb4d285ce4d90b5d3be5019c0d4d8f
MASTERALD <alexmeraz4567@gmail.com>
2023-06-15 23:18:40 -0600

    Agregar archivos de proyecto.

---
+++ PM2E146/Models/Sitios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM2E146.Models
+{
+    class Sitios
+    {
+       public int Id { get; set; }
+
+       public string Latitud { get; set; }
+
+       public string Longitud { get; set; }
+
+       public string Descripcion { get; set; }
+
+       public byte[] Imagen {get; set; }
+    }
+}
